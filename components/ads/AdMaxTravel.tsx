b98a183effe4419011dc90261242a40608223254
soysatosato <tomeiningen18go@gmail.com>
2026-03-06 11:48:55 +0900

    Add Ninja AdMax ads to attraction page

--- components/ads/AdMaxTravel.tsx
+++ components/ads/AdMaxTravel.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import Script from "next/script";
+import { useEffect, useRef } from "react";
 
 export default function AdMaxTravel() {
-  return (
-    <Script
-      src="https://adm.shinobi.jp/s/8301071f4327eeee620da1fbc6464e7d"
-      strategy="afterInteractive"
-    />
-  );
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const script = document.createElement("script");
+    script.src = "https://adm.shinobi.jp/s/8301071f4327eeee620da1fbc6464e7d";
+    script.async = true;
+
+    ref.current.appendChild(script);
+  }, []);
+
+  return <div ref={ref} className="min-h-[50px]" />;
 }
